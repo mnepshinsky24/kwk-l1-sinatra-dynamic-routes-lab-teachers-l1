2f87ed83f06ab61346c793763e82646beedebe82
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-07-17 17:13:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,29 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/:name' do
+    @user_name = params[:name]
+    @user_name
+  end
+  
+  get '/:operation/:number1/:number2' do
+    operation = params[:operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    if operation == "add"
+      answer = number1 + number2
+    elsif operation == "subtract"
+      answer = number1 - number2
+    elsif operation == "multiply"
+      answer = number1 * number2
+    elsif operation == "divide"
+      answer = number1 / number2
+    else
+      answer = "Unable to perform this operation"
+    end
+    answer.to_s
+  end
 
 end
+end
+
